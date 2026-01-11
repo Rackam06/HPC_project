0f44a29ca0c469e2421b148c7acea359651b9815
Rackam06 <wailhakam06@gmail.com>
2026-01-11 17:25:55 +0100

    fix: fixed PI number

--- code/src/bat_utils.c
+++ code/src/bat_utils.c
@@ -3,6 +3,8 @@
 #include "bat.h"
 #include "bat_utils.h"
 
+#define PI 3.14
+
 double uniform_random(double a, double b) {
     double u = (double) rand() / (double) RAND_MAX;
     return a + (b - a) * u;
@@ -28,6 +30,6 @@ double objective_function(const double x[]) {
 double normal_random(double mean, double stddev) {
     double u1 = uniform_random(0.0, 1.0);
     double u2 = uniform_random(0.0, 1.0);
-    double z0 = sqrt(-2.0 * log(u1)) * cos(2.0 * M_PI * u2);
+    double z0 = sqrt(-2.0 * log(u1)) * cos(2.0 * PI * u2);
     return mean + stddev * z0;
 }
